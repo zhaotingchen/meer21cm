ac6f33d28343e5dd5c2f905dc0def01ee7b36a76
Zhaoting Chen <ztchenustc@gmail.com>
2024-06-27 17:21:52 +0100

    more tests

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -156,3 +156,106 @@ def test_stack(test_wproj, test_W, test_nu):
     ang_mid_point = stack_3D_map.shape[0] // 2
     freq_mid_point = stack_3D_map.shape[2] // 2
     assert stack_3D_map[ang_mid_point, ang_mid_point, freq_mid_point] == 1.0
+    assert np.sum(stack_3D_map > 0) == 1
+    # test spectral direction
+    map_in = np.zeros((test_W.shape[0], test_W.shape[1], len(test_nu)))
+    map_in[indx_1, indx_2, gal_which_ch] = 0.5
+    map_in[indx_1, indx_2, gal_which_ch - 1] = 0.25
+    map_in[indx_1, indx_2, gal_which_ch + 1] = 0.25
+    (stack_3D_map, stack_3D_weight, x_edges, ang_edges,) = stack(
+        map_in,
+        test_wproj,
+        ra_in,
+        dec_in,
+        z_in,
+        test_nu,
+        W_map_in=None,
+        w_map_in=None,
+        velocity_width_halfmax=250,
+        velocity_profile="gaussian",
+        sigma_beam_in=None,
+        no_vel=False,
+        internal_step=2000,
+        verbose=False,
+        ignore_double_counting=False,
+        project_mat=None,
+        gal_sel_indx=None,
+        no_sel_weight=False,
+        stack_angular_num_nearby_pix=10,
+        # x_unit=units.km / units.s,
+        return_indx_and_weight=False,
+    )
+    assert stack_3D_map[ang_mid_point, ang_mid_point, freq_mid_point] == 0.5
+    assert stack_3D_map[ang_mid_point, ang_mid_point, freq_mid_point - 1] == 0.25
+    assert stack_3D_map[ang_mid_point, ang_mid_point, freq_mid_point + 1] == 0.25
+    assert np.sum(stack_3D_map > 0) == 3
+    # test angular
+    map_in = np.zeros((test_W.shape[0], test_W.shape[1], len(test_nu)))
+    map_in[indx_1, indx_2, gal_which_ch] = 0.6
+    map_in[indx_1 - 1, indx_2, gal_which_ch] = 0.15
+    map_in[indx_1 + 1, indx_2, gal_which_ch] = 0.05
+    map_in[indx_1, indx_2 - 1, gal_which_ch] = 0.05
+    map_in[indx_1, indx_2 + 1, gal_which_ch] = 0.15
+    (stack_3D_map, stack_3D_weight, x_edges, ang_edges,) = stack(
+        map_in,
+        test_wproj,
+        ra_in,
+        dec_in,
+        z_in,
+        test_nu,
+        W_map_in=None,
+        w_map_in=None,
+        velocity_width_halfmax=250,
+        velocity_profile="gaussian",
+        sigma_beam_in=None,
+        no_vel=False,
+        internal_step=2000,
+        verbose=False,
+        ignore_double_counting=False,
+        project_mat=None,
+        gal_sel_indx=None,
+        no_sel_weight=False,
+        stack_angular_num_nearby_pix=10,
+        # x_unit=units.km / units.s,
+        return_indx_and_weight=False,
+    )
+    assert stack_3D_map[ang_mid_point, ang_mid_point, freq_mid_point] == 0.6
+    assert stack_3D_map[ang_mid_point - 1, ang_mid_point, freq_mid_point] == 0.15
+    assert stack_3D_map[ang_mid_point + 1, ang_mid_point, freq_mid_point] == 0.05
+    assert stack_3D_map[ang_mid_point, ang_mid_point - 1, freq_mid_point] == 0.05
+    assert stack_3D_map[ang_mid_point, ang_mid_point + 1, freq_mid_point] == 0.15
+
+    # test averaging
+    ra_in = np.array([350.0, 340.0])
+    dec_in = np.array([-27.0, -24.0])
+    z_in = np.array([0.45, 0.4505])
+    indx_1, indx_2 = radec_to_indx(ra_in, dec_in, test_wproj)
+    gal_freq = f_21 / (1 + z_in) / 1e6
+    gal_which_ch = np.argmin(np.abs(gal_freq[None, :] - test_nu[:, None]), axis=0)
+    map_in = np.zeros((test_W.shape[0], test_W.shape[1], len(test_nu)))
+    map_in[indx_1[0], indx_2[0], gal_which_ch[0]] = 1.5
+    map_in[indx_1[1], indx_2[1], gal_which_ch[1]] = 0.5
+    (stack_3D_map, stack_3D_weight, x_edges, ang_edges,) = stack(
+        map_in,
+        test_wproj,
+        ra_in,
+        dec_in,
+        z_in,
+        test_nu,
+        W_map_in=None,
+        w_map_in=None,
+        velocity_width_halfmax=250,
+        velocity_profile="gaussian",
+        sigma_beam_in=None,
+        no_vel=False,
+        internal_step=2000,
+        verbose=False,
+        ignore_double_counting=False,
+        project_mat=None,
+        gal_sel_indx=None,
+        no_sel_weight=False,
+        stack_angular_num_nearby_pix=10,
+        # x_unit=units.km / units.s,
+        return_indx_and_weight=False,
+    )
+    assert stack_3D_map[ang_mid_point, ang_mid_point, freq_mid_point] == 1.0
