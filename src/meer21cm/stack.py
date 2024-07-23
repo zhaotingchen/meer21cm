fix units

src/meer21cm/stack.py
@@ -27,6 +27,7 @@ def weight_source_peaks(
     project_mat=None,
     gal_sel_indx=None,
     no_sel_weight=False,
+    ang_unit="deg",
 ):
     if W_map_in is None:
         W_map_in = np.ones_like(map_in)
@@ -34,6 +35,8 @@ def weight_source_peaks(
         w_map_in = np.ones_like(map_in)
     num_ch = map_in.shape[-1]
     num_g = ra_g_in.size
+    ra_g_in = (ra_g_in * units.Unit(ang_unit)).to("deg").value
+    dec_g_in = (dec_g_in * units.Unit(ang_unit)).to("deg").value
     if ignore_double_counting:
         return np.zeros(map_in.shape, dtype="int") - 1, W_map_in
     xx, yy = np.meshgrid(
@@ -67,7 +70,7 @@ def weight_source_peaks(
     else:
         raise ValueError("Unrecognised velocity profile: " + str(velocity_profile))
 
-    gal_freq = f_21 / (1 + z_g_in) / 1e6
+    gal_freq = f_21 / (1 + z_g_in)
     # which channel each source centre belongs to
     gal_which_ch = np.argmin(np.abs(gal_freq[None, :] - nu[:, None]), axis=0)
 
@@ -194,6 +197,7 @@ def stack(
     stack_angular_num_nearby_pix=10,
     x_unit=units.km / units.s,
     return_indx_and_weight=False,
+    ang_unit="deg",
 ):
     # calculate what pixels to stack
     map_gal_indx, map_gal_weight = weight_source_peaks(
@@ -215,6 +219,7 @@ def stack(
         project_mat=project_mat,
         gal_sel_indx=gal_sel_indx,
         no_sel_weight=no_sel_weight,
+        ang_unit=ang_unit,
     )
     if W_map_in is None:
         W_map_in = np.ones_like(map_in)
@@ -222,13 +227,15 @@ def stack(
         w_map_in = np.ones_like(map_in)
     num_ch = map_in.shape[-1]
     num_g = ra_g_in.size
+    ra_g_in = (ra_g_in * units.Unit(ang_unit)).to("deg").value
+    dec_g_in = (dec_g_in * units.Unit(ang_unit)).to("deg").value
     xx, yy = np.meshgrid(
         np.arange(map_in.shape[0]), np.arange(map_in.shape[1]), indexing="ij"
     )
     # the coordinates of each pixel in the map
     ra, dec = get_wcs_coor(wproj, xx, yy)
 
-    gal_freq = f_21 / (1 + z_g_in) / 1e6
+    gal_freq = f_21 / (1 + z_g_in)
     # which channel each source centre belongs to
     gal_which_ch = np.argmin(np.abs(gal_freq[None, :] - nu[:, None]), axis=0)
     # in deg^2
@@ -460,7 +467,7 @@ def sum_3d_stack(stack_3D_map, vel_ch_avg=5, ang_sum_dist=3.0):
 #        hiprofile_g = profile_func(vel_int_edges)
 #        hiprofile_g /= np.sum(hiprofile_g)
 #
-#    gal_freq = f_21/(1+z_g_in)/1e6
+#    gal_freq = f_21/(1+z_g_in)
 #    # which channel each source centre belongs to
 #    gal_which_ch = np.argmin(np.abs(gal_freq[None,:]-nu[:,None]),axis=0)
 #

src/meer21cm/util.py
@@ -127,7 +127,7 @@ def read_healpix_fits(file):
     return hp_map, hp_nside, map_unit, map_freq
 
 
-def get_wcs_coor(wcs, xx, yy):
+def get_wcs_coor(wcs, xx, yy, ang_unit="deg"):
     """
     Retrieve RA and Dec coordinates of pixels in the WCS.
 
@@ -149,8 +149,8 @@ def get_wcs_coor(wcs, xx, yy):
     """
     assert wcs.naxis == 2, "input wcs must be 2-dimensional."
     coor = wcs.pixel_to_world(xx, yy)
-    ra = coor.ra.deg
-    dec = coor.dec.deg
+    ra = getattr(coor.ra, ang_unit)
+    dec = getattr(coor.dec, ang_unit)
     return ra, dec
 
 
@@ -213,8 +213,8 @@ def pcaclean(
     return Residual
 
 
-def radec_to_indx(ra_arr, dec_arr, wproj, to_int=True):
-    coor = SkyCoord(ra_arr, dec_arr, unit="deg")
+def radec_to_indx(ra_arr, dec_arr, wproj, to_int=True, ang_unit="deg"):
+    coor = SkyCoord(ra_arr, dec_arr, unit=ang_unit)
     indx_1, indx_2 = wproj.world_to_pixel(coor)
     if to_int:
         indx_1 = np.round(indx_1).astype("int")
@@ -279,10 +279,18 @@ def find_rotation_matrix(vec):
     return rot_mat_2 @ rot_mat_1
 
 
-def minimum_enclosing_box_of_lightcone(ra_arr, dec_arr, freq, cosmo=Planck18):
+def minimum_enclosing_box_of_lightcone(
+    ra_arr,
+    dec_arr,
+    freq,
+    cosmo=Planck18,
+    ang_unit="deg",
+):
     """
     not really minimum but should be okay.
     """
+    ra_arr = (ra_arr * units.Unit(ang_unit)).to("deg").value
+    dec_arr = (dec_arr * units.Unit(ang_unit)).to("deg").value
     ra_temp = ra_arr.copy()
     ra_temp[ra_temp > 180] -= 360
     ra_mean = ra_temp.mean()
@@ -302,7 +310,17 @@ def minimum_enclosing_box_of_lightcone(ra_arr, dec_arr, freq, cosmo=Planck18):
     return (x_min, y_min, z_min, x_max - x_min, y_max - y_min, z_max - z_min, inv_rot)
 
 
-def hod_obuljen18(logmh, m0h=9.52, mminh=11.27, alpha=0.44, cosmo=Planck18):
-    marr = 10**logmh  # in Msun/h
+def hod_obuljen18(
+    logmh,
+    m0h=9.52,
+    mminh=11.27,
+    alpha=0.44,
+    cosmo=Planck18,
+    input_has_h=True,
+    output_has_h=False,
+):
+    input_h_unit = 1.0 if input_has_h else cosmo.h
+    output_h_unit = 1.0 if output_has_h else 1 / cosmo.h
+    marr = 10**logmh * input_h_unit  # in Msun/h
     himass = 10 ** (m0h) * (marr / 10**mminh) ** alpha * np.exp(-(10**mminh) / marr)
-    return himass / cosmo.h
+    return himass * output_h_unit

tests/conftest.py
@@ -56,7 +56,7 @@ def test_W():
 @pytest.fixture
 def test_nu():
     file = data_dir + "test_nu.npy"
-    return np.load(file)
+    return np.load(file) * 1e6
 
 
 @pytest.fixture

tests/test_mock.py
@@ -212,8 +212,8 @@ def test_gal_pos_in_mock(
     assert np.mean(dec_g[inside_range] > test_GAMA_range[1][0]) == 1
     assert np.mean(dec_g[inside_range] < test_GAMA_range[1][1]) == 1
     # test z_g_mock distribution is not needed as the underlying function is tested in hiimtool
-    assert (f_21 / 1e6 / (1 + z_g) >= nu_edges[gal_which_ch]).mean() == 1
-    assert (f_21 / 1e6 / (1 + z_g) <= nu_edges[gal_which_ch + 1]).mean() == 1
+    assert (f_21 / (1 + z_g) >= nu_edges[gal_which_ch]).mean() == 1
+    assert (f_21 / (1 + z_g) <= nu_edges[gal_which_ch + 1]).mean() == 1
     # ra,dec mapping to index should be tested in util
     # when no velocity, the flux should be one channel
     assert len(hifluxd_in) == 1
@@ -249,10 +249,10 @@ def test_gal_pos_in_mock(
             y_dim=test_W.shape[1],
             ignore_double_counting=False,
             return_indx_and_weight=False,
-            fix_z=f_21 / 1e6 / test_nu.mean() - 1,
+            fix_z=f_21 / test_nu.mean() - 1,
             fix_ra_dec=(350, -30),
         )
-        assert z_g == f_21 / 1e6 / test_nu.mean() - 1
+        assert z_g == f_21 / test_nu.mean() - 1
         assert ra_g == 350
         assert dec_g == -30
         assert (himap_g > 0).sum() == len(hifluxd_in[hifluxd_in > 0])
@@ -341,7 +341,7 @@ def test_plt(test_mock_func, test_wproj, test_W, test_nu, test_GAMA_range):
         y_dim=test_W.shape[1],
         ignore_double_counting=False,
         return_indx_and_weight=False,
-        fix_z=np.ones(num_g) * f_21 / 1e6 / test_nu.mean() - 1,
+        fix_z=np.ones(num_g) * f_21 / test_nu.mean() - 1,
         fix_ra_dec=(np.ones(num_g) * 350, np.ones(num_g) * (-30.0)),
     )
     plt.close("all")
@@ -389,8 +389,8 @@ def test_mock_healpix(test_wproj, test_W, test_nu, test_GAMA_range):
     assert np.mean(dec_g[inside_range] > test_GAMA_range[1][0]) == 1
     assert np.mean(dec_g[inside_range] < test_GAMA_range[1][1]) == 1
     # test z_g_mock distribution is not needed as the underlying function is tested in hiimtool
-    assert (f_21 / 1e6 / (1 + z_g) >= nu_edges[gal_which_ch]).mean() == 1
-    assert (f_21 / 1e6 / (1 + z_g) <= nu_edges[gal_which_ch + 1]).mean() == 1
+    assert (f_21 / (1 + z_g) >= nu_edges[gal_which_ch]).mean() == 1
+    assert (f_21 / (1 + z_g) <= nu_edges[gal_which_ch + 1]).mean() == 1
     # ra,dec mapping to index should be tested in util
     # when no velocity, the flux should be one channel
     assert len(hifluxd_in) == 1
@@ -426,7 +426,7 @@ def test_mock_healpix(test_wproj, test_W, test_nu, test_GAMA_range):
         y_dim=test_W.shape[1],
         ignore_double_counting=False,
         return_indx_and_weight=False,
-        fix_z=f_21 / 1e6 / nu.mean() - 1,
+        fix_z=f_21 / nu.mean() - 1,
         fast_ang_pos=False,
     )
     assert (himap_g > 0).sum() == len(hifluxd_in[hifluxd_in > 0])

tests/test_stack.py
@@ -15,7 +15,7 @@ def test_weight_source_peaks(test_wproj, test_W, test_nu):
     ra_in = np.array([350.0])
     dec_in = np.array([-25.0])
     z_in = np.array([0.45])
-    gal_freq = f_21 / (1 + z_in) / 1e6
+    gal_freq = f_21 / (1 + z_in)
     indx_1, indx_2 = radec_to_indx(ra_in, dec_in, test_wproj)
     # which channel each source centre belongs to
     gal_which_ch = np.argmin(np.abs(gal_freq[None, :] - test_nu[:, None]), axis=0)
@@ -83,7 +83,7 @@ def test_weight_source_peaks(test_wproj, test_W, test_nu):
     ra_in = np.array([350.0, 350.0])
     dec_in = np.array([-27.0, -27.0])
     z_in = np.array([0.45, 0.4505])
-    gal_freq = f_21 / (1 + z_in) / 1e6
+    gal_freq = f_21 / (1 + z_in)
     indx_1, indx_2 = radec_to_indx(ra_in, dec_in, test_wproj)
     map_gal_indx, map_gal_weight = weight_source_peaks(
         map_in,
@@ -118,7 +118,7 @@ def test_stack(test_wproj, test_W, test_nu):
     ra_in = np.array([350.0])
     dec_in = np.array([-25.0])
     z_in = np.array([0.45])
-    gal_freq = f_21 / (1 + z_in) / 1e6
+    gal_freq = f_21 / (1 + z_in)
     indx_1, indx_2 = radec_to_indx(ra_in, dec_in, test_wproj)
     # which channel each source centre belongs to
     gal_which_ch = np.argmin(np.abs(gal_freq[None, :] - test_nu[:, None]), axis=0)
@@ -230,7 +230,7 @@ def test_stack(test_wproj, test_W, test_nu):
     dec_in = np.array([-27.0, -24.0])
     z_in = np.array([0.45, 0.4505])
     indx_1, indx_2 = radec_to_indx(ra_in, dec_in, test_wproj)
-    gal_freq = f_21 / (1 + z_in) / 1e6
+    gal_freq = f_21 / (1 + z_in)
     gal_which_ch = np.argmin(np.abs(gal_freq[None, :] - test_nu[:, None]), axis=0)
     map_in = np.zeros((test_W.shape[0], test_W.shape[1], len(test_nu)))
     map_in[indx_1[0], indx_2[0], gal_which_ch[0]] = 1.5