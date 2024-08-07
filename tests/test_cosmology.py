fce1efc888ea68caf56bb09954121001efb2a913
Zhaoting Chen <ztchenustc@gmail.com>
2024-08-07 22:00:03 +0100

    better observer pattern

--- tests/test_cosmology.py
+++ tests/test_cosmology.py
@@ -1,5 +1,5 @@
 from meer21cm.cosmology import CosmologyCalculator
-from astropy.cosmology import Planck18
+from astropy.cosmology import Planck18, Planck15
 import numpy as np
 
 
@@ -9,3 +9,14 @@ def test_cosmo():
     # test input sigma8 resulting in consistent As with Planck18
     coscal.camb_pars
     assert np.abs(np.log(1e10 * coscal.As) - 3.047) < 1e-3
+
+
+def test_update_pars():
+    coscal = CosmologyCalculator()
+    coscal.camb_pars
+    coscal.cosmo = "Planck15"
+    coscal.cosmo
+    As = coscal.As
+    coscal.camb_pars
+    assert coscal.h == Planck15.h
+    assert coscal.As != As
