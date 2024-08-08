809f9c68d58fa5875956ad0f26e07f24c38c7b4b
Zhaoting Chen <ztchenustc@gmail.com>
2024-08-08 22:23:43 +0100

    add pipeline module

---
+++ tests/test_pipeline.py
@@ -0,0 +1,31 @@
+import numpy as np
+from meer21cm.pipeline import MockObservation
+from meer21cm.mock import lamb_21
+from astropy import constants, units
+
+
+def test_unit_convert(test_nu, test_wproj):
+    mockobs = MockObservation(
+        test_nu,
+        test_wproj,
+        seed=42,
+    )
+    jy_2_k = mockobs.jy_to_kelvin
+    pix_area = mockobs.pix_area
+    z_ch = mockobs.z_ch
+    k_2_jy = (
+        (
+            (2 * constants.k_B * units.K / (lamb_21 * (1 + (z_ch))) ** 2)
+            * (pix_area * np.pi**2 / 180**2)
+        )
+        .to("Jy")
+        .value
+    )
+    assert np.allclose(jy_2_k, 1 / k_2_jy)
+    assert mockobs.freq_to_vel(mockobs.freq_resol) == mockobs.vel_resol
+    assert mockobs.vel_to_freq(mockobs.vel_resol) == mockobs.freq_resol
+
+
+def test_noise_sigma(test_nu, test_wproj):
+    mockobs = MockObservation(test_nu, test_wproj, seed=42, time_resol=2)
+    sigma_n = mockobs.thermal_noise_sigma()
