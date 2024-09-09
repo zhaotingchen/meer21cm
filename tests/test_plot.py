4f864ebbe97aa9c318b6094a77414b5ff5c831e7
Zhaoting Chen <ztchenustc@gmail.com>
2024-09-09 14:40:36 +0100

    fix array passing

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -7,4 +7,5 @@ def test_plt(test_W, test_nu, test_wproj):
     plot_pixels_along_los(test_W, test_W, zaxis=test_nu[:1])
     plot_eigenspectrum(np.array([1, 2]))
     plot_map(test_W, test_wproj, W=test_W, vmin=0, vmax=1)
+    plot_map(test_W, test_wproj, vmin=0, vmax=1)
     plt.close("all")
